1fd6e029f4792ea4039cec0987f93daa297692d6
Zac <zachary.xl.hah@gmail.com>
2025-01-02 16:53:52 +1000

    basic drive code
    should drive with same setup except with PWM TalonSRX because CAN TalonSRX was deprecated :(

--- docs.md
+++ docs.md
@@ -1,7 +1,6 @@
 # Unqualified Quokkas Ri3D 2025 Software Documentation #
 
 This document covers the engineering process and decisions made on the robot software and control systems.
-
 Please ask questions through the [ChiefDelphi thread](https://www.chiefdelphi.com/t/2025-unqualified-quokkas-ri3d-build-thread/476780).
 
 ## Overview ##
@@ -10,11 +9,15 @@ in progress
 ---
 
 ## Journal ##
+### 02 Jan 2024 ###
+Happy new year,
+Going to go out of my comfort zone and try [Command Based](https://docs.wpilib.org/en/stable/docs/software/commandbased/index.html#command-based-programming). Hopefully it'll make auto programming clearer?
+Worst case we just [copy 254](https://github.com/Team254/FRC-2024-Public/tree/main). :)
+
+ZH
+
 ### 31 Dec 2024 ###
-Setting up environment to make template with this https://docs.wpilib.org/en/stable/docs/yearly-overview/returning-quickstart.html
-
-We are using 2025 tools so that it's easier for 5985 to do a week 0 scrimmage later on.
-
-Hoping to use [this tool](https://dillinger.io/) to format a nice PDF at the end.
+Starting with this https://docs.wpilib.org/en/stable/docs/yearly-overview/returning-quickstart.html
+We are using 2025 tools so that it's easier for 5985 to do a week 0 scrimmage later on. 
 
 ZH
---
+++ testbench/TestBench/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+  // Left
+  private final PWMTalonSRX m_left1 = new PWMTalonSRX(DriveConstants.kLeftMotor1Port);
+  private final PWMTalonSRX m_left2 = new PWMTalonSRX(DriveConstants.kLeftMotor2Port);
+
+  // Right
+  private final PWMTalonSRX m_right1 = new PWMTalonSRX(DriveConstants.kRightMotor1Port);
+  private final PWMTalonSRX m_right2 = new PWMTalonSRX(DriveConstants.kRightMotor2Port);
+
+  // Define drive by leaders of motor groups
+  private final DifferentialDrive m_drive =
+      new DifferentialDrive(m_left1::set, m_right1::set);
+
+  // Left encoder
+  private final Encoder m_leftEncoder =
+      new Encoder(
+          DriveConstants.kLeftEncoderPorts[0],
+          DriveConstants.kLeftEncoderPorts[1],
+          DriveConstants.kLeftEncoderReversed);
+
+  // Right encoder
+  private final Encoder m_rightEncoder =
+      new Encoder(
+          DriveConstants.kRightEncoderPorts[0],
+          DriveConstants.kRightEncoderPorts[1],
+          DriveConstants.kRightEncoderReversed);
+
+  public DriveSubsystem() {
+    SendableRegistry.addChild(m_drive, m_left1);
+    SendableRegistry.addChild(m_drive, m_right1);
+
+    m_left1.addFollower(m_left2);
+    m_right1.addFollower(m_right2);
+
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_right1.setInverted(true);
+
+    // Sets the distance per pulse for the encoders
+    m_leftEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+    m_rightEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+  }
+
+  /**
+   * Drives the robot using arcade controls.
+   *
+   * @param fwd the commanded forward movement
+   * @param rot the commanded rotation
+   */
+  public void arcadeDrive(double fwd, double rot) {
+    m_drive.arcadeDrive(fwd, rot);
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+  }
+
+  /**
+   * Gets the average distance of the TWO encoders.
+   *
+   * @return the average of the TWO encoder readings
+   */
+  public double getAverageEncoderDistance() {
+    return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
+  }
+
+  /**
+   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
+   *
+   * @param maxOutput the maximum output to which the drive will be constrained
+   */
+  public void setMaxOutput(double maxOutput) {
+    m_drive.setMaxOutput(maxOutput);
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    super.initSendable(builder);
+    // Publish encoder distances to telemetry.
+    builder.addDoubleProperty("leftDistance", m_leftEncoder::getDistance, null);
+    builder.addDoubleProperty("rightDistance", m_rightEncoder::getDistance, null);
+  }
+}
