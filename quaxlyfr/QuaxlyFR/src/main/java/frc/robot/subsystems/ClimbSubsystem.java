a93fed49a78caa99fe9f264c1e1983a22e4c865c
Zac <zachary.xl.hah@gmail.com>
2025-01-10 14:13:21 +1000

    Deleted old Quaxly and Testbench

--- quaxlyfr/QuaxlyFR/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ quaxlyfr/QuaxlyFR/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -9,7 +9,6 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.LiftConstants;
 import frc.robot.util.UqUtil;
 import frc.robot.Constants.ClimbConstants;
 
